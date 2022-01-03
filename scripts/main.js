916bef14484fb60851cad616075fe1657c0f5b50
Chamek0 <ressegairedamien@gmail.com>
2022-01-03 11:34:14 +0100

    damscan: initial realese

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+/*
+let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Webtoon';
+
+/*let myImage = document.querySelector('img');
+
+myImage.addEventListener('click', function() {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/tb.jpg') {
+      myImage.setAttribute('src', 'images/pepe.jpg');
+    } else {
+      myImage.setAttribute('src', 'images/tb.jpg');
+    }
+});*/
+
+
